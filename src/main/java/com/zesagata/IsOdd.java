49799efe6c180f28570d58e63c7fda579a8cf942
zesagata <zezagata17@gmail.com>
2020-12-14 11:37:39 +0700

    initial commit

---
+++ src/main/java/com/zesagata/IsOdd.java
@@ -0,0 +1,9 @@
+package com.zesagata;
+
+public class IsOdd {
+
+  public static boolean isOdd(int number) {
+    return number % 2 == 1;
+  }
+
+}
---
+++ src/test/java/com/zesagata/TestIsOdd.java
@@ -0,0 +1,13 @@
+package com.zesagata;
+
+import org.junit.jupiter.api.Test;
+
+public class TestIsOdd {
+
+  @Test public void test() {
+    int number = 9;
+
+    System.out.println(IsOdd.isOdd(number));
+  }
+
+}
